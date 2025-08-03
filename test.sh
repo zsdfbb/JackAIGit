8296e7bdda32700bd99b252571098fccf1980009
Zhang Shuai <zsdfbb@126.com>
2025-08-03 12:50:45 +0800

    feat(aigit-integration): add robust installation checks and test management for aigit in test.sh
    Signed-off-by: Zhang Shuai <zsdfbb@126.com>

--- test.sh
+++ test.sh
@@ -3,3 +3,30 @@
 set -e
 
 echo "Run AIGit test..."
+
+command -v aigit >/dev/null 2>&1 || {
+  echo >&2 "aigit is not installed. Aborting."
+  exit 1
+}
+
+# =============================================================
+# prepare test
+echo "# test" > ./install.sh
+
+# =============================================================
+# do test
+
+aigit show -e 5965fdf805c4c81bd709980ab2ba2b12f04fbf19
+
+aigit diff -e
+
+git add ./
+aigit diff -e -s
+
+aigit commit -e -d
+
+# =============================================================
+# restore
+git reset --soft HEAD^
+git restore --staged ./install.sh
+git checkout -- ./install.sh
